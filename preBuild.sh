a9dfb56ff4bdf1bcf6a5835b4fd46566398ae834
smile921 <ruby2010ruby@163.com>
2016-03-24 17:13:23 +0800

    ss

---
+++ preBuild.sh
@@ -0,0 +1,17 @@
+
+
+rm -rf posts/*.html
+sed -i 's/\r$//' posts/*
+
+sed -i 's/\r$//' js/*
+
+sed -i 's/\r$//' css/*
+
+rm -rf _site
+rm -rf _cache
+mkdir _site/
+
+cp -rfv js _site/
+cp -rfv font _site/
+cp -rfv images _site/
+cp -rfv js _site/
