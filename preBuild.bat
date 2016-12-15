414ee03203a7db0bcfbfde885124f8a453efba19
Frère Jacques <zyb@zjex.com>
2016-12-15 16:48:16 +0800

    up

---
+++ preBuild.bat
@@ -0,0 +1,20 @@
+chcp 65001
+rem site.exe build  
+ 
+rm -rf posts/*.html
+sed -i 's/\r$//' posts/*
+
+sed -i 's/\r$//' js/*
+
+sed -i 's/\r$//' css/*
+
+rm -rf _site
+rm -rf _cache
+mkdir _site/
+site.exe build  
+cp -rfv js _site/
+cp -rfv font _site/
+cp -rfv images _site/
+cp -rfv js _site/
+cp -fv README.md _site/
+pause
